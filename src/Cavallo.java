4837f0a4e467d9118318ceccde30c1c05fec14dd
gaia-mincigrucci <gaia.mincigrucci@gmail.com>
2025-10-24 11:11:13 +0200

    Cavallo.java

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -1,48 +1,44 @@
-import javax.naming.InterruptedNamingException;
+/*questa classe è il nostro primo thread e consentirà la simulazione di un cavallo in corsa parallelamente con altri della stessa specie
+ * @author brugiati
+ * @version 1.0
+ * */
+public class Cavallo extends Thread{
 
-/** questa classe è il nostro primo thread e consentirà la simulazione di un cavallo in corsa
- * parellelamente con altri della stessa specie
- * @author gaia
- * @version 1.0
+    private final String name;
+    private int lentezza;
+    private boolean interrotto = false;
+    
+    public Cavallo(String name, int lentezza) {
+        super();
+        this.name = name;
+        this.lentezza = lentezza;
+    }
+    /**override del metodo run */
+    @Override
+    public void run(){
+        System.out.println("Cavallo " +
+                name + " comincia il suo galoppo");
+        for (int i = 1; i <= 10 && !Thread.currentThread().isInterrupted(); i++) {
+            try {
+                sleep(lentezza);
+            } catch (InterruptedException e) {
+                interrotto = true;
+                System.out.println(name + " è stato interrotto!");
+                return;
+            }
+            System.out.println(name +" cavalca - passo: " + i);
+        }
+        if(Main.getPrimo().equals("") && !interrotto){
+            Main.setPrimo(this.name);
+        }
+    }
 
- */
-public class Cavallo extends Thread {
-    private String name;
-    private int lentezza;
+    protected int getLentezza(){
+        return lentezza;
+    }
 
-    public Cavallo(String name, int lentezza) {
-        super();
-        this.name = name;
-        this.lentezza = lentezza;
-    }
-
-    public int getLentezza() {
-        return lentezza;
-
-    }
-
-    public void setLentezza(int lentezza) {
-        this.lentezza = lentezza;
-    }
-
-
-    /**
-     * override del metodo run
-     */
-    @Override
-    public void run() {
-
-        System.out.println("Cavallo" + name + "comincia il suo galoppo.");
-        for (int i = 1; i <= 10; i++) {
-            try {
-                sleep(lentezza);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            System.out.println(name + "cavalca - passo " + i);
-
-
-        }
-    }
+    protected void setLentezza(int lentezza){
+        this.lentezza=lentezza;
+    }
 }
+
