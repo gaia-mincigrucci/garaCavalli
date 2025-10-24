8676eb8e5a74d6f2b2d81d0340318dfa0e3a9257
gaia-mincigrucci <gaia.mincigrucci@gmail.com>
2025-10-24 13:23:52 +0200

    Cavallo.java

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -1,5 +1,5 @@
 /*questa classe è il nostro primo thread e consentirà la simulazione di un cavallo in corsa parallelamente con altri della stessa specie
- * @author brugiati
+ * @author mincigrucci
  * @version 1.0
  * */
 public class Cavallo extends Thread{
